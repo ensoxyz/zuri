93efcfc9458fc42ad1ad002a407713d670b3c525
ensoxyz <ceo@enso.xyz>
2021-01-20 21:11:39 -0600

    +

--- _4/superflat.ts
+++ _4/superflat.ts
@@ -4,6 +4,16 @@ let h:number = 0;
 let s:number = 96;
 let l:number = 50;
 
+let shades:number = 3;
+let tones:number = 3;
+let tints:number = 3;
+
+let instance:number = 9;
+let state:number = 9;
+
+let color:number = instance * state;
+
+
 
 function hsl(){
 }
